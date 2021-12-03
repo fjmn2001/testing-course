654a745ee7c93fa26a0f7ecea96ede0c376f8385
🚀 Francisco Marcano <fjmn2001@gmail.com>
2021-12-03 16:02:00 -0400

    feat: adding example from workshop v2. Ref sn

--- src/Santiago/ManhattanDistanceCalcultator.php
+++ src/Santiago/ManhattanDistanceCalcultator.php
@@ -9,22 +9,23 @@ use Medine\Santiago\Exceptions\DistanceErrorException;
 
 final class ManhattanDistanceCalcultator
 {
+    private int $i = 0;
+
     public function __invoke(array $vector1, array $vector2): int
     {
         if (count($vector1) !== count($vector2)) {
             throw new DistanceErrorException;
         }
 
-        $i = 0;
-
-        return array_reduce($vector1, $this->toReduce($vector2, $i), 0);
+        return array_reduce($vector1, $this->toReduce($vector2), 0);
     }
 
-    private function toReduce(array $vector2, int $i): Closure
+    private function toReduce(array $vector2): Closure
     {
-        return static function (int $acc, int $value) use ($vector2, &$i) {
-            $i++;
-            return $acc + abs($value - $vector2[$i - 1]);
+        return function (int $acc, int $value) use ($vector2) {
+            $this->i++;
+
+            return $acc + abs($value - $vector2[$this->i - 1]);
         };
     }
 }
