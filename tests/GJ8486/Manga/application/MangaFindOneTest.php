9488ddb5d5919cdb185d96da83432c0f3cf60fad
gabrielsalazar8486 <salazargabriel8486@gmail.com>
2021-12-13 14:22:11 -0400

    Unit test with mock

--- tests/GJ8486/Manga/application/MangaFindOneTest.php
+++ tests/GJ8486/Manga/application/MangaFindOneTest.php
@@ -4,7 +4,8 @@ namespace Tests\GJ8486\Manga\application;
 
 use Medine\GJ8486\Manga\Application\MangaFindOne;
 use Medine\GJ8486\Manga\Domain\Manga;
-use Medine\GJ8486\Manga\Infrastructure\MangaPersistenceText;
+use Medine\GJ8486\Manga\Domain\MangaPersistence;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class MangaFindOneTest extends TestCase
@@ -13,12 +14,12 @@ class MangaFindOneTest extends TestCase
     /** @test */
     public function itShouldFindAndReturnAManga()
     {
-        $DB = new MangaPersistenceText;
-        $DB->save((Manga::create('20202020202', 'Vinland Saga', 'Makoto Yukimura')));
-        $DB->save((Manga::create('20202020200', 'One Piece', 'Eiichirō Oda')));
-        $DB->save((Manga::create('20202020201', 'Death Note', 'Tsugumi Ōba')));
+        $repository = Mockery::mock(MangaPersistence::class);
+        $repository->shouldReceive('findOne')
+            ->withArgs(['20202020200'])
+            ->andReturn(Manga::create('20202020200', 'One Piece', 'Eiichirō Oda'));
 
-        $findOne = new MangaFindOne($DB);
+        $findOne = new MangaFindOne($repository);
 
         $result = $findOne('20202020200');
         $this->assertEquals('One Piece', $result->nombre());
